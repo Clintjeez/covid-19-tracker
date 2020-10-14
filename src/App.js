4592571032b0ddf8049731ebff1d5b8ba167139d
clintjeez <clintjeez@gmail.com>
2020-10-14 04:40:28 +0100

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "NIGERIA"]);
+
+  // https://disease.sh/v3/covid-19/countries
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+            {/* 
+            <MenuItem value="worldwide">option 1</MenuItem>
+            <MenuItem value="worldwide">option 2</MenuItem>
+            <MenuItem value="worldwide">option 3</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
