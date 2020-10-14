5795be7a6b16dda93aca7d1bfed623ee9db07642
clintjeez <clintjeez@gmail.com>
2020-10-14 05:08:52 +0100

    header setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 import "./App.css";
 
 function App() {
-  const [countries, setCountries] = useState(["USA", "UK", "NIGERIA"]);
+  const [countries, setCountries] = useState([]);
 
   // https://disease.sh/v3/covid-19/countries
 
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-            {/* 
-            <MenuItem value="worldwide">option 1</MenuItem>
-            <MenuItem value="worldwide">option 2</MenuItem>
-            <MenuItem value="worldwide">option 3</MenuItem> */}
           </Select>
         </FormControl>
       </div>
